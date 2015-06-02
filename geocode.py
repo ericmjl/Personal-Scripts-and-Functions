364b883bed0e82dfb1adaee1989d52d987ea75b5
Eric Ma <ericmajinglong@gmail.com>
2015-06-02 11:41:26 -0400

    Updated comments for geocode

--- geocode.py
+++ geocode.py
@@ -2,34 +2,33 @@
 Author: Eric J. Ma
 
 Purpose of Code: 
-This code is a code snippet that shows how to search for GPS longitude 
-and latitude coordinates based on a given set of search terms.
-
+This is a function that gets the longitude and latitude of a search term. 
 """
 
 from geopy.geocoders import GeoNames, GoogleV3
 
-def get_geo_coordinates(search_terms):
+def get_lon_lat_coordinates(search_term, engine):
 	"""
-	Input: 
-	An iterable of terms to be searched. The search terms 
-	should be country|locale, separated by a vertical pipe.
-	Output: A list of tuples, where each tuple comprises the:
-		- search term
-		- returned location string based on the search term
-		- longitude
-		- latitude
-	"""
-	geocoder = GoogleV3(timeout=5)
-	geodata = []
-	for search_term in search_terms:
-		new_search_term = search_term.replace('|', ' ')
-		loc = geocoder.geocode(new_search_term)
-		if loc is None:
-			loc = geocoder.geocode(search_term.split('|')[0])
-		lat = loc.latitude
-		lon = loc.longitude
-		data = (search_term, u'{0}'.format(loc), lon, lat)
-		geodata.append(data)
+	Gets the longitude and latitude of a specified search term.
+	
+	Parameters:
+	===========
 
-	return geodata
+	-	search_term:	string. The name of the place for which you want to search.
+	-	engine:			string. Currently supports:
+						- 'geonames'
+						- 'google'
+
+	Returns:
+	========
+	-	(lon, lat)		tuple of floats. The longitude and latitude coordinates.
+
+	"""
+	gc = {'geonames':GeoNames(country_bias='USA', username='ericmjl'),
+		  'google':GoogleV3(timeout=5)}
+	geocoder = gc[engine]
+	loc = geocoder.geocode(search_term)
+	lat = loc.latitude
+	lon = loc.longitude
+
+	return lon, lat
